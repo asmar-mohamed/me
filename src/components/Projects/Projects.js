4aa6f8b29e33f9c07430b8e723136e605cc240fa
asmar-mohamed <mohamed3asmar222@gmail.com>
2026-02-11 17:16:19 +0100

    pngs Fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import mission from "../../Assets/Projects/mission.png";
+import reco from "../../Assets/Projects/reco.png";
+import bus from "../../Assets/Projects/bus.png";
 
 function Projects() {
   return (
@@ -19,7 +20,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={bus}
               isBlog={false}
               title="Transport Presence Management"
               description="Web platform for managing presence in urban transport using PHP and MySQL. Allows real-time registration of passenger presence via QR Code, consultation of presence lists, and real-time bus tracking by the administrator."
@@ -30,7 +31,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={reco}
               isBlog={false}
               title="Course Recommendation Platform"
               description="Intelligent course recommendation platform using React, TypeScript, and Redux. Full-stack web application for personalized learner orientation. Design and development of an innovative AI-based web solution recommending courses adapted to user skills and objectives."
@@ -40,13 +41,13 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}  // ضع هنا الصورة المناسبة
+              imgPath={mission}  
               isBlog={false}
               title="Gestion Mission Parking"
               description="A web application to manage parking missions efficiently. 
     Users can create, update, and monitor missions, track vehicles, filter by status or date, and visualize active and completed missions on a dashboard. Built with React and Bootstrap for a responsive and user-friendly interface."
               ghLink="https://github.com/Mahmoudlammadni/Gestion_mission_parking"
-              // demoLink="#" // ضع رابط Live Demo بعد deploy
+              // demoLink="#"
             />
           </Col>
 
