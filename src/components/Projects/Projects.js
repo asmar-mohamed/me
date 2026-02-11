bd89a72289a84b31ce5bf90df170622415f0e1f3
asmar-mohamed <mohamed3asmar222@gmail.com>
2026-02-11 15:44:00 +0100

    add project Gestion Mission Parking

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,7 +23,7 @@ function Projects() {
               isBlog={false}
               title="Transport Presence Management"
               description="Web platform for managing presence in urban transport using PHP and MySQL. Allows real-time registration of passenger presence via QR Code, consultation of presence lists, and real-time bus tracking by the administrator."
-            // ghLink="https://github.com/asmar-mohamed/..."
+            ghLink="https://github.com/asmar-mohamed/bus_tracking_system_project"
             // demoLink="https://..."
             />
           </Col>
@@ -34,10 +34,22 @@ function Projects() {
               isBlog={false}
               title="Course Recommendation Platform"
               description="Intelligent course recommendation platform using React, TypeScript, and Redux. Full-stack web application for personalized learner orientation. Design and development of an innovative AI-based web solution recommending courses adapted to user skills and objectives."
-            // ghLink="https://github.com/asmar-mohamed/..."
+            ghLink="https://github.com/asmar-mohamed/AI-Course-Recommendation"
             // demoLink="https://..."
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}  // ضع هنا الصورة المناسبة
+              isBlog={false}
+              title="Gestion Mission Parking"
+              description="A web application to manage parking missions efficiently. 
+    Users can create, update, and monitor missions, track vehicles, filter by status or date, and visualize active and completed missions on a dashboard. Built with React and Bootstrap for a responsive and user-friendly interface."
+              ghLink="https://github.com/Mahmoudlammadni/Gestion_mission_parking"
+              // demoLink="#" // ضع رابط Live Demo بعد deploy
+            />
+          </Col>
+
         </Row>
       </Container>
     </Container>
